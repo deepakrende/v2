6345bccc8e1c0f126f014d2896f5a699f5263c29
deepakrende <deepakrende@gmail.com>
2026-09-11 17:21:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,6 +12,7 @@ from database.users_chats_db import db, delete_all_referal_users, get_referal_us
 from database.join_reqs import JoinReqs
 from info import CLONE_MODE, OWNER_LNK, CHANNELS, REQUEST_TO_JOIN_MODE, TRY_AGAIN_BTN, ADMINS, SHORTLINK_MODE, PREMIUM_AND_REFERAL_MODE, STREAM_MODE, AUTH_CHANNEL, REFERAL_PREMEIUM_TIME, REFERAL_COUNT, PAYMENT_TEXT, PAYMENT_QR, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHNL_LNK, GRP_LNK, REQST_CHANNEL, SUPPORT_CHAT, MAX_B_TN, VERIFY, SHORTLINK_API, SHORTLINK_URL, TUTORIAL, VERIFY_TUTORIAL, IS_TUTORIAL, URL, CUSTOM_MSG
 from utils import get_settings, pub_is_subscribed, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, get_shortlink, get_tutorial, get_seconds
+from plugins.pm_filter import start_movie_request
 from database.connections_mdb import active_connection
 from urllib.parse import quote_plus
 from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
@@ -120,6 +121,11 @@ async def start(client, message):
             print(e)
             return await message.reply_text("something wrong with force subscribe.")
             
+    if len(message.command) == 2 and message.command[1] == "reqmovie":
+        await message.reply("<b>🎬 Lᴇᴛ's ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛ sᴇɴᴛ ᴛᴏ ᴀᴅᴍɪɴ!</b>")
+        await start_movie_request(client, message.from_user.id, message.from_user.mention)
+        return
+
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         if PREMIUM_AND_REFERAL_MODE == True:
             buttons = [[
